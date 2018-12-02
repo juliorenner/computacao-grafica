ecef9b2d801fe4544c4df69336a4a7ba62a1f979
Julio Renner <julio.renner@hotmail.com>
2018-12-02 17:47:22 -0200

    Adjust Z with color

--- Object Writer/Object Writer/main.cpp
+++ Object Writer/Object Writer/main.cpp
@@ -14,6 +14,8 @@
 #include <vector>
 #include <math.h>
 
+#define BUFSIZE 512
+
 using namespace std;
 
 GLFWwindow *window;
@@ -21,10 +23,15 @@ GLFWwindow *window;
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-Shader* mainShader;
+Shader* cpShader;
+Shader* curvesShader;
 
 GLuint CP_VBO, CP_SPLINE_VBO, INT_VBO, EXT_VBO;
 GLuint CP_VAO, CP_SPLINE_VAO, INT_VAO, EXT_VAO;
+
+// Modes: (E) edit = 0 (D) draw = 1 (Z) select mode = 2
+GLuint renderMode = 0;
+GLuint selectedPointIndex = -15;
 
 vector<float> cpVertix, cpSplineVertix, iVertix, eVertix;
 
@@ -35,7 +42,13 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
 void processInput(GLFWwindow *window);
 void calculateControlPointSpline();
 void calculateInternalAndExternalCurves();
-void load(GLuint VAO, GLuint VBO, vector<float> &vertix);
+int getNearestPoint(double x, double y);
+void selectPoint(double x, double y);
+void updateZIndex(bool isIncreasing);
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods);
+void loadCurves(GLuint VAO, GLuint VBO, vector<float> &vertix);
+void loadControlPoints();
+
 
 int main(int argc, const char * argv[]) {
     if (!glfwInit ()) {
@@ -63,6 +76,7 @@ int main(int argc, const char * argv[]) {
     glfwMakeContextCurrent (window);
     glfwSetWindowSizeCallback(window, framebuffer_size_callback);
     glfwSetMouseButtonCallback(window, mouse_button_callback);
+    glfwSetKeyCallback(window, key_callback);
     
     // inicia manipulador da extensão GLEW
     glewExperimental = GL_TRUE;
@@ -81,8 +95,13 @@ int main(int argc, const char * argv[]) {
 
     glGenBuffers(1, &EXT_VBO);
     glGenVertexArrays(1, &EXT_VAO);
+    
+//    glGenBuffers(1, &CP_COLOR_VBO);
 
-    mainShader = new Shader("vs.glsl", "fs.glsl");
+    curvesShader = new Shader("vs.glsl", "fs.glsl");
+    cpShader = new Shader("vs_cp.glsl", "fs_cp.glsl");
+    
+    glPointSize(12);
     
     // render loop
     // -----------
@@ -97,24 +116,28 @@ int main(int argc, const char * argv[]) {
         glClearColor(1.0f, 1.0f, 1.0f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
         
-        // draw our first triangle
-        mainShader->use();
-        
-        mainShader->setMat4("projection", projection);
-    
-        glBindVertexArray(CP_VAO);
-        glDrawArrays(GL_LINE_STRIP, 0, cpVertix.size()/2);
-        
-        if(cpVertix.size() >= 8) {
+        if (renderMode != 1) {
+            cpShader->use();
+            cpShader->setMat4("projection", projection);
             
+            glBindVertexArray(CP_VAO);
+            glDrawArrays(GL_POINTS, 0, cpVertix.size()/6);
+            glDrawArrays(GL_LINE_STRIP, 0, cpVertix.size()/6);
+        }
+        
+        if(cpVertix.size() >= 12) {
+            curvesShader->use();
+            curvesShader->setMat4("projection", projection);
             glBindVertexArray(CP_SPLINE_VAO);
-            glDrawArrays(GL_LINE_STRIP, 0, cpSplineVertix.size()/2);
+            glDrawArrays(GL_LINE_STRIP, 0, cpSplineVertix.size()/3);
 
-            glBindVertexArray(INT_VAO);
-            glDrawArrays(GL_LINE_STRIP, 0, iVertix.size()/2);
-            
-            glBindVertexArray(EXT_VAO);
-            glDrawArrays(GL_LINE_STRIP, 0, eVertix.size()/2);
+            if (renderMode == 1) {
+                glBindVertexArray(INT_VAO);
+                glDrawArrays(GL_LINE_STRIP, 0, iVertix.size()/3);
+                
+                glBindVertexArray(EXT_VAO);
+                glDrawArrays(GL_LINE_STRIP, 0, eVertix.size()/3);
+            }
         }
         
         glBindVertexArray(0);
@@ -142,6 +165,51 @@ void processInput(GLFWwindow *window)
         glfwSetWindowShouldClose(window, true);
 }
 
+void key_callback(GLFWwindow* window, int key, int scancode, int action, int mods)
+{
+    if (key == GLFW_KEY_E && action == GLFW_PRESS)
+        renderMode = 0;
+    if (key == GLFW_KEY_D && action == GLFW_PRESS)
+    {
+        renderMode = 1;
+        calculateInternalAndExternalCurves();
+        
+        loadCurves(INT_VAO, INT_VBO, iVertix);
+        loadCurves(EXT_VAO, EXT_VBO, eVertix);
+    }
+    if (key == GLFW_KEY_Z && action == GLFW_PRESS) {
+        selectedPointIndex = -15;
+        renderMode = 2;
+    }
+    if ((key == GLFW_KEY_UP || key == GLFW_KEY_DOWN) && action == GLFW_PRESS && renderMode == 2 && selectedPointIndex >= 0) {
+        updateZIndex(key == GLFW_KEY_UP);
+        loadControlPoints();
+    }
+}
+
+void updateZIndex(bool isIncreasing) {
+    if (isIncreasing) {
+        if (cpVertix[selectedPointIndex + 5] > 0)
+            cpVertix[selectedPointIndex + 5] -= 0.1;
+        else if (cpVertix[selectedPointIndex + 4] < 1){
+            cpVertix[selectedPointIndex + 4] += 0.1;
+        }
+    } else {
+        if (cpVertix[selectedPointIndex + 4] > 0)
+            cpVertix[selectedPointIndex + 4] -= 0.1;
+        else if (cpVertix[selectedPointIndex + 5] < 1)
+            cpVertix[selectedPointIndex + 5] += 0.1;
+    }
+    float zIndex = cpVertix[selectedPointIndex + 4] - cpVertix[selectedPointIndex + 5];
+    if (zIndex > 1) {
+        zIndex = 1.0f;
+    } else if (zIndex < -1){
+        zIndex = -1.0f;
+    }
+    
+    cpVertix[selectedPointIndex + 2] = zIndex;
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     // make sure the viewport matches the new window dimensions; note that width and
@@ -157,40 +225,69 @@ void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
         //getting cursor position
         glfwGetCursorPos(window, &xpos, &ypos);
         
-        cpVertix.push_back((float)xpos);
-        cpVertix.push_back((float)ypos);
-        
-        if(cpVertix.size() >= 8 && cpVertix.size() % 2 == 0) {
-            calculateControlPointSpline();
-            calculateInternalAndExternalCurves();
-            load(CP_SPLINE_VAO, CP_SPLINE_VBO, cpSplineVertix);
-            load(INT_VAO, INT_VBO, iVertix);
-            load(EXT_VAO, EXT_VBO, eVertix);
+        if (renderMode == 0) {
+            cpVertix.push_back((float)xpos);
+            cpVertix.push_back((float)ypos);
+            cpVertix.push_back(0);
+            cpVertix.push_back(1);
+            cpVertix.push_back(0);
+            cpVertix.push_back(0);
         }
         
-        load(CP_VAO, CP_VBO, cpVertix);
+        if (renderMode == 2) {
+            selectPoint(xpos, ypos);
+        }
+        
+        if(cpVertix.size() >= 12) {
+            calculateControlPointSpline();
+            loadCurves(CP_SPLINE_VAO, CP_SPLINE_VBO, cpSplineVertix);
+        }
+        
+        loadControlPoints();
     }
+    
+    glRenderMode(GL_RENDER);
+}
+
+void selectPoint(double x, double y) {
+    selectedPointIndex = getNearestPoint(x, y);
+    loadControlPoints();
+}
+
+int getNearestPoint(double x, double y) {
+    int nearest = 0;
+    double distance = INFINITY;
+    for(int i = 0; i < cpVertix.size(); i += 6) {
+        double d = sqrt( pow((cpVertix[i]-x),2) + pow(cpVertix[i+1],2) );
+        if (d < distance) {
+            distance = d;
+            nearest = i;
+        }
+    }
+    
+    return nearest;
 }
 
 void calculateControlPointSpline() {
     cpSplineVertix.clear();
     double j = 0.05;
-    for (int i = 0; i < cpVertix.size(); i+=2) {
+    for (int i = 0; i < cpVertix.size(); i+=6) {
         for (double t = 0; t <= 1; t+=j) {
             float x = ((
                         (-1*pow(t,3) +3*pow(t,2) -3*t +1) * (cpVertix[i]) +
-                        ( 3*pow(t,3) -6*pow(t,2) +0*t +4) * (cpVertix[(i+2) % cpVertix.size()]) +
-                        (-3*pow(t,3) +3*pow(t,2) +3*t +1) * (cpVertix[(i+4) % cpVertix.size()]) +
-                        ( 1*pow(t,3) +0*pow(t,2) +0*t +0) * (cpVertix[(i+6) % cpVertix.size()])
+                        ( 3*pow(t,3) -6*pow(t,2) +0*t +4) * (cpVertix[(i+(6*1)) % cpVertix.size()]) +
+                        (-3*pow(t,3) +3*pow(t,2) +3*t +1) * (cpVertix[(i+(6*2)) % cpVertix.size()]) +
+                        ( 1*pow(t,3) +0*pow(t,2) +0*t +0) * (cpVertix[(i+(6*3)) % cpVertix.size()])
                     )/6);
             float y = ((
                         (-1*pow(t,3) +3*pow(t,2) -3*t +1) * (cpVertix[i+1]) +
-                        ( 3*pow(t,3) -6*pow(t,2) +0*t +4) * (cpVertix[(i+3) % cpVertix.size()]) +
-                        (-3*pow(t,3) +3*pow(t,2) +3*t +1) * (cpVertix[(i+5) % cpVertix.size()]) +
-                        ( 1*pow(t,3) +0*pow(t,2) +0*t +0) * (cpVertix[(i+7) % cpVertix.size()])
+                        ( 3*pow(t,3) -6*pow(t,2) +0*t +4) * (cpVertix[((i+1)+(6*1)) % cpVertix.size()]) +
+                        (-3*pow(t,3) +3*pow(t,2) +3*t +1) * (cpVertix[((i+1)+(6*2)) % cpVertix.size()]) +
+                        ( 1*pow(t,3) +0*pow(t,2) +0*t +0) * (cpVertix[((i+1)+(6*3)) % cpVertix.size()])
                         )/6);
             cpSplineVertix.push_back(x);
             cpSplineVertix.push_back(y);
+            cpSplineVertix.push_back(0);
         }
     }
 }
@@ -198,12 +295,12 @@ void calculateControlPointSpline() {
 void calculateInternalAndExternalCurves() {
     iVertix.clear();
     eVertix.clear();
-    for (int i = 0; i < cpSplineVertix.size(); i+=2) {
+    for (int i = 0; i < cpSplineVertix.size(); i+=3) {
         double ax = cpSplineVertix[i % cpSplineVertix.size()];
         double ay = cpSplineVertix[(i+1) % cpSplineVertix.size()];
         
-        double bx = cpSplineVertix[(i+2) % cpSplineVertix.size()];
-        double by = cpSplineVertix[(i+3) % cpSplineVertix.size()];
+        double bx = cpSplineVertix[(i+3) % cpSplineVertix.size()];
+        double by = cpSplineVertix[(i+4) % cpSplineVertix.size()];
         
         double w = bx - ax;
         double h = by - ay;
@@ -229,22 +326,43 @@ void calculateInternalAndExternalCurves() {
         
         iVertix.push_back(iCX);
         iVertix.push_back(iCY);
+        iVertix.push_back(0);
         
         eVertix.push_back(eCX);
         eVertix.push_back(eCY);
+        eVertix.push_back(0);
     }
     
 }
 
-void load(GLuint VAO, GLuint VBO, vector<float> &vertix)
+void loadCurves(GLuint VAO, GLuint VBO, vector<float> &vertix)
 {
     glBindVertexArray(VAO);
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, vertix.size()*sizeof(float), vertix.data(), GL_STATIC_DRAW);
 
     glEnableVertexAttribArray(0);
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), NULL);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), NULL);
 
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+    glBindVertexArray(0);
+}
+
+void loadControlPoints()
+{
+    glBindVertexArray(CP_VAO);
+    glBindBuffer(GL_ARRAY_BUFFER, CP_VBO);
+    glBufferData(GL_ARRAY_BUFFER, cpVertix.size()*sizeof(float), cpVertix.data(), GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)0);
+    glEnableVertexAttribArray(0);
+    
+//    glBindBuffer(GL_ARRAY_BUFFER, CP_COLOR_VBO);
+//    glBufferData(GL_ARRAY_BUFFER, cpColors.size()*sizeof(float), cp`.data(), GL_STATIC_DRAW);
+    
+    glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, 6 * sizeof(float), (void*)(3 * sizeof(float)));
+    glEnableVertexAttribArray(1);
+    
     glBindBuffer(GL_ARRAY_BUFFER, 0);
     glBindVertexArray(0);
 }
--- Object Writer/Object Writer/vs_cp.glsl
+++ Object Writer/Object Writer/vs_cp.glsl
@@ -1,9 +1,13 @@
-#version 410 core
+#version 330 core
 layout (location = 0) in vec3 aPos;
+layout (location = 1) in vec3 aColors;
 
 uniform mat4 projection;
+
+out vec3 color;
 
 void main()
 {
     gl_Position = projection * vec4(aPos.x, aPos.y, aPos.z, 1.0);
+    color = aColors;
 }
